c63864aca575dc8f41d20bf251d3e54ba6dd0603
xinhehappy <3308394371@qq.com>
2016-07-10 20:39:23 +0800

    从服务器端更新菜品信息，验证用户名密码。

--- SCOS/app/src/main/java/es/source/code/activity/LoginOrRegister.java
+++ SCOS/app/src/main/java/es/source/code/activity/LoginOrRegister.java
@@ -8,13 +8,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.MalformedInputException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import es.source.code.model.User;
 
 /**
+ *
  * Created by Administrator on 2016/6/15.
  */
 public class LoginOrRegister extends Activity{
@@ -26,6 +36,7 @@ public class LoginOrRegister extends Activity{
     Button register = null;
     EditText userNameEt = null;
     EditText passwordEt = null;
+    boolean flag = false;
 
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
@@ -62,31 +73,47 @@ public class LoginOrRegister extends Activity{
             public void onClick(View v) {
                 userName = userNameEt.getText().toString();
                 password = passwordEt.getText().toString();
-                Log.i("LoginOrRegister",userName);
-                if(userName.isEmpty() || !check(userName)){
-                    userNameEt.setError("输入内容不符合规则");
-                    return;
-                }else if(password.isEmpty() || !check(password)){
-                    passwordEt.setError("输入内容不符合规则");
-                    return;
-                }else{
 
-                    editor.putString("userName",userName);//用户名写入SharedPreferences
-                    editor.putInt("loginState",1);
-                    editor.commit();
+                //使用HttpURLConnection访问SCOSSever的Servlet类LoginValidator
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        flag = checkUserOnServlet(userName,password);
+                    }
+                }){
 
-                    User loginUser = new User();
-                    loginUser.setUserName(userName);
-                    loginUser.setPassword(password);
-                    loginUser.setOldUser(true);
-                    Intent intent = new Intent(LoginOrRegister.this,MainScreen.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putSerializable("loginUser",loginUser);
-                    intent.putExtra("value","LoginSuccess");
-                    intent.putExtras(bundle);
-                    LoginOrRegister.this.startActivity(intent);
-                    LoginOrRegister.this.finish();
+                }.start();
+                if(flag)
+                {
+                    Log.i("LoginOrRegister",userName);
+                    if(userName.isEmpty() || !check(userName)){
+                        userNameEt.setError("输入内容不符合规则");
+                        return;
+                    }else if(password.isEmpty() || !check(password)){
+                        passwordEt.setError("输入内容不符合规则");
+                        return;
+                    }else{
+
+                        editor.putString("userName",userName);//用户名写入SharedPreferences
+                        editor.putInt("loginState",1);
+                        editor.commit();
+
+                        User loginUser = new User();
+                        loginUser.setUserName(userName);
+                        loginUser.setPassword(password);
+                        loginUser.setOldUser(true);
+                        Intent intent = new Intent(LoginOrRegister.this,MainScreen.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putSerializable("loginUser",loginUser);
+                        intent.putExtra("value","LoginSuccess");
+                        intent.putExtras(bundle);
+                        LoginOrRegister.this.startActivity(intent);
+                        LoginOrRegister.this.finish();
+                        Toast.makeText(getBaseContext(),"服务器验证用户成功",Toast.LENGTH_LONG).show();
+                    }
                 }
+
+
 
             }
         });
@@ -135,6 +162,44 @@ public class LoginOrRegister extends Activity{
             }
         });
     }
+    public static boolean checkUserOnServlet(String userName,String password){
+        //将用户名密码放入HashMap中
+        String path = "http://192.168.100.109:8080/SCOSSever/";
+        HashMap<String,String> params = new HashMap<String,String>();
+        params.put("userName",userName);
+        params.put("password",password);
+        try{
+            return sendPostRequest(path,params,"UTF-8");
+        } catch (MalformedURLException me){
+            me.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public static boolean sendPostRequest(String path,HashMap<String,String> params,String encode) throws MalformedURLException,IOException{
+
+        StringBuilder url = new StringBuilder(path);
+        url.append("?");
+        for(Map.Entry<String,String> entry:params.entrySet()){
+            url.append(entry.getKey()).append("=");
+            url.append(URLEncoder.encode(entry.getValue(),encode));
+            url.append("&");
+        }
+        //删除掉最后一个&
+        url.deleteCharAt(url.length()-1);
+        HttpURLConnection con = (HttpURLConnection)new URL(url.toString()).openConnection();
+        con.setConnectTimeout(5000);
+        con.setRequestMethod("POST");
+        if(con.getResponseCode() == 200){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
     public static boolean check(String str){
         String regex = "^[a-zA-Z0-9]+$";
         Pattern p = Pattern.compile(regex);
--- SCOS/app/src/main/java/es/source/code/service/UpdateService.java
+++ SCOS/app/src/main/java/es/source/code/service/UpdateService.java
@@ -8,8 +8,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import es.source.code.activity.FoodDetailed;
 import es.source.code.activity.FoodsOnService;
@@ -45,8 +55,9 @@ public class UpdateService extends IntentService {
             Context context = getApplicationContext();
             CharSequence contentTitle = "notification";
             FoodsOnService foodItem = mFoodOnService.get(0);
-            CharSequence contentText = "新品上架:"+mFoodOnService.get(0).getFoodName()
-                    +"价格："+mFoodOnService.get(0).getFoodPrice()+"类型："+mFoodOnService.get(0).getType();
+//            CharSequence contentText = "新品上架:"+mFoodOnService.get(0).getFoodName()
+//                    +"价格："+mFoodOnService.get(0).getFoodPrice()+"类型："+mFoodOnService.get(0).getType();
+            CharSequence contentText = "新品上架："+"菜品数量 10"+ "    清除";
             Intent notificationIntent = new Intent(getBaseContext(), FoodDetailed.class);
             Bundle bundle = new Bundle();
             bundle.putSerializable("newFood",foodItem);
@@ -70,5 +81,47 @@ public class UpdateService extends IntentService {
     }
     public UpdateService(){
         super("UpdateService");
+    }
+
+    public static boolean checkUserOnServlet(String userName,String password){
+        String path = "http://192.168.100.109:8080/SCOSSever/";
+        HashMap<String,String> params = new HashMap<String,String>();
+        //解析json数据
+        JSONObject json  = new JSONObject();
+        try {
+            json.get("foods");
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+        try{
+            return sendPostRequest(path,params,"UTF-8");
+        } catch (MalformedURLException me){
+            me.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public static boolean sendPostRequest(String path,HashMap<String,String> params,String encode) throws MalformedURLException,IOException{
+
+        StringBuilder url = new StringBuilder(path);
+        url.append("?");
+        for(Map.Entry<String,String> entry:params.entrySet()){
+            url.append(entry.getKey()).append("=");
+            url.append(URLEncoder.encode(entry.getValue(),encode));
+            url.append("&");
+        }
+        //删除掉最后一个&
+        url.deleteCharAt(url.length()-1);
+        HttpURLConnection con = (HttpURLConnection)new URL(url.toString()).openConnection();
+        con.setConnectTimeout(5000);
+        con.setRequestMethod("POST");
+        if(con.getResponseCode() == 200){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
